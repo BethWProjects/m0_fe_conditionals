4c283e2d02284fb89b66c8b624834281a1a2ea94
Beth Wilson <bethwilson777@gmail.com>
2022-06-08 17:27:40 -0600

    Adding decision Making answers

--- decisionMaking.js
+++ decisionMaking.js
@@ -35,9 +35,18 @@ if (bearChoice === 1) {
 
 // Questions
 // 1. In English, using technical vocabulary, describe what is happening between lines 12 and 16.
+// Lines 12 - 16 is an if/else statement.  The conditions set on line 12 will determine if the code on line 13 or 15 will run.
 // 2. What variable has a new value assigned to it after the first if statement executes?
+//bearClothing
 // 3. If you changed the variable doorChoice to equal 3, what would the bearClothing value be?
+// hat
 // 4. In English, using technical vocabulary, describe what is happening between lines lines 25 and 33.
+// Lines 25 - 33 are using if/else statements depending on the conditions set.  If the bears choice is door 1 or 2,
+// the code will use contatanation to combine two strings and a variable.
+// If the bear chooses door 3 with the condition bearChoice === 3, the code is a string, if it is neither door 1, 2, or 3,
+// you see a string explaining your the bears friends.
 // 5. If you changed the variable bearChoice to equal 3, what will be the final outcome be?
+// "You run as fast as you can into the next room. It's full of snakes!";
 // 6. If you changed the variable doorChoice to equal 1, and the variable bearChoice to equal 2, what will be the final outcome be?
-// 7. What is your favorite ending?
+// "You tell the bear the hat is too small and it starts to cry!";
+// 7. What is your favorite ending?   "You stay with the bear and become it's best friend!";
