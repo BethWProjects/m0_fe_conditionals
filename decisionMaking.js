9f5450b4b3395bf2fcc162070e99478259531a23
Beth Wilson <bethwilson777@gmail.com>
2022-06-08 19:48:29 -0600

    Adding a  command

--- decisionMaking.js
+++ decisionMaking.js
@@ -23,7 +23,7 @@ console.log("2. Point it out to the bear?");
 console.log("3. Make a dash for the next room?");
 
 if (bearChoice === 1) {
-  "You offer the bear your " + bearClothing + " and the bear shows you a secret passage out!";
+  puts "You offer the bear your " + bearClothing + " and the bear shows you a secret passage out!";
 } else if (bearChoice === 2) {
   "You tell the bear the " + bearClothing + " is too small and it starts to cry!";
 } else if (bearChoice === 3) {
